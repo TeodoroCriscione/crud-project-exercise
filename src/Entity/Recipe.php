d9d52136d62bf0538e3eb567ec93cad578359ecc
Teodoro Criscione <teodoro.criscione@gmail.com>
2025-04-15 13:41:51 +0200

    Initialize CRUD project

---
+++ src/Entity/Recipe.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $ingredients = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $instructions = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getIngredients(): ?string
+    {
+        return $this->ingredients;
+    }
+
+    public function setIngredients(string $ingredients): static
+    {
+        $this->ingredients = $ingredients;
+
+        return $this;
+    }
+
+    public function getInstructions(): ?string
+    {
+        return $this->instructions;
+    }
+
+    public function setInstructions(string $instructions): static
+    {
+        $this->instructions = $instructions;
+
+        return $this;
+    }
+}
---
+++ src/Form/RecipeType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recipe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', null, [
+                'attr' => ['class'=> 'form-control my-2', 'placeholder'=>'Recipe Name']
+            ])
+            ->add('ingredients',null, [
+                'attr' => ['class'=> 'form-control my-2', 'placeholder'=>'Recipe Ingredients']
+            ])
+            ->add('instructions',null, [
+                'attr' => ['class'=> 'form-control my-2', 'placeholder'=>'Recipe Instructions']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipe::class,
+        ]);
+    }
+}
